88817b50aa5899f092d114b4b8c8d25f95af8495
xXxRosexXxEE <rose@the-flower-garden.co.uk>
2013-12-04 15:31:27 +0000

    fixing still

--- Stages.cpp
+++ Stages.cpp
@@ -8,12 +8,10 @@
 
 #ifndef _STAGES_CPP_
 #define _STAGES_CPP_
-
-bool Stage::One(bool Switch)
-{
-    Player.LeftMove(0,0,true);
-    Player.RightMove(0,0,true);
-    Player.Jump(0,0,true);
-    SDL_BlitSurface(GameSurface::PlayerSurface, NULL, GameSurface::MainSurface, &MoveMent);
-};
+		
+		bool Stage::One(bool Switch)
+		{
+			Player.Spawn(0,0,true);
+			SDL_BlitSurface(GameSurface::PlayerSurface, NULL, GameSurface::MainSurface, &MoveMent);
+		};
 #endif
